500c290d066f6ae5415767044624c3a26d1f7262
Juan Miguel <juanmigue2408gmail.com>
2026-03-11 22:41:58 -0500

    Initial commit - random quote generator

---
+++ README.md
@@ -0,0 +1,22 @@
+\# Python Random Quote Generator
+
+
+
+Simple Python script that prints a random motivational quote.
+
+
+
+\## How to run
+
+
+
+python quote\_generator.py
+
+
+
+\## Technologies
+
+
+
+Python
+
---
+++ quote-generator.py
@@ -0,0 +1,14 @@
+import random
+
+quotes = [
+    "Success is not final, failure is not fatal.",
+    "Do something today that your future self will thank you for.",
+    "Small progress is still progress.",
+    "Dream big and dare to fail.",
+    "Discipline beats motivation."
+]
+
+quote = random.choice(quotes)
+
+print("Quote of the day:")
+print(quote)
